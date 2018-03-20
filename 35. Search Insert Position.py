67335f77b717789fb1fd432dbe8322a9f7fb27d6
Chad Chai <345391607@qq.com>
2018-03-19 20:22:45 -0500

    Update 35. Search Insert Position.py

--- 35. Search Insert Position.py
+++ 35. Search Insert Position.py
@@ -38,13 +38,14 @@ class Solution(object):
 '''
 Solution 2:
 Binary Search
+Use len()-1 to avoid index overfitting.
 '''
 class Solution(object):
     def searchInsert(self, nums, target):
         low = 0
         high = len(nums) - 1
         while low <= high:
-            mid = (low + high) / 2
+            mid = (low + high) // 2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
