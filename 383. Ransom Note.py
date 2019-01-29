ef0acf596297f82f91638832f40c8330a010d5cc
Chad Chai <Jinchao.chai@gmail.com>
2019-01-29 15:29:15 -0600

    Update 383. Ransom Note.py

--- 383. Ransom Note.py
+++ 383. Ransom Note.py
@@ -1,5 +1,5 @@
 '''
-
+Tag: String; Difficulty: Easy;
 Given an arbitrary ransom note string and another string containing letters from all the magazines,
 write a function that will return true if the ransom note can be constructed from the magazines ; 
 otherwise, it will return false.
