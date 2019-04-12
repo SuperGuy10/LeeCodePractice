3fa19aaeb0e8cfd8cc7631812ebb60a4905524f9
Chad Chai <Jinchao.chai@gmail.com>
2019-04-11 21:04:42 -0500

    Create 197.Rising Temperature.sql

---
+++ 197.Rising Temperature.sql
@@ -0,0 +1,39 @@
+/*
+Given a Weather table, write a SQL query to find all dates' Ids with higher temperature compared to its previous (yesterday's) dates.
+
++---------+------------------+------------------+
+| Id(INT) | RecordDate(DATE) | Temperature(INT) |
++---------+------------------+------------------+
+|       1 |       2015-01-01 |               10 |
+|       2 |       2015-01-02 |               25 |
+|       3 |       2015-01-03 |               20 |
+|       4 |       2015-01-04 |               30 |
++---------+------------------+------------------+
+For example, return the following Ids for the above Weather table:
+
++----+
+| Id |
++----+
+|  2 |
+|  4 |
++----+
+*/
+
+
+# Write your MySQL query statement below
+SELECT B.Id
+FROM Weather AS A, Weather AS B
+WHERE TO_DAYS(B.RecordDate) - TO_DAYS(A.RecordDate) = 1 # change date value to days number in a year
+AND B.Temperature > A.Temperature
+
+
+# Solution 2
+SELECT
+    weather.id AS 'Id'
+FROM
+    weather
+        JOIN
+    weather w ON DATEDIFF(weather.date, w.date) = 1
+        AND weather.Temperature > w.Temperature
+        
+# EATEDIFF(DATE1,DATE2) = VALUE :use to finde the difference between two date values
