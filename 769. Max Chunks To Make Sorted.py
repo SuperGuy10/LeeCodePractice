58f5ece8941aae0c294a49c9b87160c9f2d397a9
Chad Chai <Jinchao.chai@gmail.com>
2019-05-12 14:52:06 -0500

    Update 769. Max Chunks To Make Sorted.py

--- 769. Max Chunks To Make Sorted.py
+++ 769. Max Chunks To Make Sorted.py
@@ -22,6 +22,16 @@ Note:
 arr will have length in range [1, 10].
 arr[i] will be a permutation of [0, 1, ..., arr.length - 1].
 '''
+'''
+The key to understand this algorithms lies in the fact that when max[index] == index, 
+all the numbers before index must be smaller than max[index] (also index), 
+so they make up of a continuous unordered sequence, i.e {0,1,..., index}. 
+This is because numbers in array only vary in range [0, 1, ..., arr.length - 1], 
+so the most numbers you can find that are smaller than a certain number, 
+say arr[k], would be arr[k] - 1, i.e [0, 1, ..., arr[k] - 1]. So when arr[k] is the max number in [arr[0], arr[1], ..., arr[k]],
+all the k - 1 numbers before it can only lies in [0, 1, ..., arr[k] - 1], so they made up of a continuous sequence. 
+(You can also prove it using contradiction, which may be easier to understand)
+'''
 
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
